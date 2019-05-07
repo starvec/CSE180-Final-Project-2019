68c4fa53a5fe35f3714b052f449ad5416f7094f4
Jeffrey Kazmierski <jefkazmer@gmail.com>
2019-05-07 12:59:59 -0700

    avoids obstacles barriers

--- finalmove.cpp
+++ finalmove.cpp
@@ -22,8 +22,43 @@ bool receivedTableLegs = false;
 bool receivedMailBoxes = false;
 bool needToCancelGoal = false;
 
+signed char getGlobalValueAt(int x, int y) {
+    return global.data.at((y/global.info.resolution + 400)*global.info.width + (x/global.info.resolution + 400));
+}
 
-void createGoalPoints();
+void createGoalPoints()
+{
+    move_base_msgs::MoveBaseGoal goalTemp;
+
+    goalTemp.target_pose.header.frame_id = "map";
+    goalTemp.target_pose.header.stamp = Time::now();
+    goalTemp.target_pose.pose.orientation.x = 0;
+    goalTemp.target_pose.pose.orientation.y = 0;
+    goalTemp.target_pose.pose.orientation.w = 0.7071068;
+
+    int mod = 1;
+
+    for (int i = -8; i <= 8; i += 4)
+    {
+        goalTemp.target_pose.pose.position.x = i;
+        
+        for (int j = -8; j <= 8; j += 4)
+        {
+            if (getGlobalValueAt(i, j*mod) < 10)
+            {
+                goalTemp.target_pose.pose.position.y = j*mod;
+                goalTemp.target_pose.pose.orientation.z = 0.7071068*mod;
+                goal.push_back(goalTemp);
+                sentPose.push_back(false);
+            }
+            else
+            {
+                ROS_INFO_STREAM("Value at (" << i << ", " << j*mod << ") is " << (int)(getGlobalValueAt(i, j*mod)));
+            }
+        }
+        mod *= -1;
+    }
+}
 
 void receiveGlobalMap(const nav_msgs::OccupancyGrid &msg)
 {
@@ -45,16 +80,15 @@ void receiveMailBoxCloud(const sensor_msgs::PointCloud &msg)
 
 void serviceActivated()
 {
-    ROS_INFO_STREAM("Service received goal point (" << goal.at(currentPoseIndex).target_pose.pose.position.x << ", " 
+    ROS_INFO_STREAM("Received goal point (" << goal.at(currentPoseIndex).target_pose.pose.position.x << ", " 
                         << goal.at(currentPoseIndex).target_pose.pose.position.y << ")");
 }
 
 void serviceDone(const actionlib::SimpleClientGoalState& state, const move_base_msgs::MoveBaseResultConstPtr& result)
 {
-    ROS_INFO_STREAM("Service completed");
     if (state.SUCCEEDED)
     {
-        ROS_INFO_STREAM("Service reached goal point (" << goal.at(currentPoseIndex).target_pose.pose.position.x << ", " 
+        ROS_INFO_STREAM("Reached goal point (" << goal.at(currentPoseIndex).target_pose.pose.position.x << ", " 
                             << goal.at(currentPoseIndex).target_pose.pose.position.y << ")");
 
         if (currentPoseIndex < goal.size()-1)
@@ -68,7 +102,8 @@ void serviceDone(const actionlib::SimpleClientGoalState& state, const move_base_
     }
     else if (state.ABORTED)
     {
-        ROS_INFO_STREAM("Aborted current goal");
+        ROS_INFO_STREAM("Aborted goal point (" << goal.at(currentPoseIndex).target_pose.pose.position.x << ", " 
+                            << goal.at(currentPoseIndex).target_pose.pose.position.y << ")");
         if (currentPoseIndex < goal.size()-1)
             currentPoseIndex++;
         else
@@ -85,26 +120,24 @@ void serviceFeedback(const move_base_msgs::MoveBaseFeedbackConstPtr& fb)
     //For every table leg
     for (int i = 0; i < tableLegs.points.size(); i++)
     {
-        //If distance between the current goal and this table leg is < 2 and the distance between the current goal and the husky is < 3...
+        //If distance between the current goal and this table leg is < 1 and the distance between the current goal and the husky is < 2...
         if (sqrt(pow(goal.at(currentPoseIndex).target_pose.pose.position.x - tableLegs.points.at(i).x, 2) + pow(goal.at(currentPoseIndex).target_pose.pose.position.y - tableLegs.points.at(i).y, 2)) < 1 &&
                 sqrt(pow(goal.at(currentPoseIndex).target_pose.pose.position.x - fb->base_position.pose.position.x, 2) + pow(goal.at(currentPoseIndex).target_pose.pose.position.y - fb->base_position.pose.position.y, 2)) < 2)
                 {
                     //...cancel the current goal
                     needToCancelGoal = true;
-                    //ROS_INFO_STREAM("Cancelling goal at (" << goal.at(currentPoseIndex).target_pose.pose.position.x << ", " << goal.at(currentPoseIndex).target_pose.pose.position.y << "because a table leg is in the way");
                 }
     }
     
     //For every mail box
     for (int i = 0; i < mailBoxes.points.size(); i++)
     {
-        //If distance between the current goal and this mail box is < 2 and the distance between the current goal and the husky is < 3...
+        //If distance between the current goal and this mail box is < 1 and the distance between the current goal and the husky is < 2...
         if (sqrt(pow(goal.at(currentPoseIndex).target_pose.pose.position.x - mailBoxes.points.at(i).x, 2) + pow(goal.at(currentPoseIndex).target_pose.pose.position.y - mailBoxes.points.at(i).y, 2)) < 1 &&
                 sqrt(pow(goal.at(currentPoseIndex).target_pose.pose.position.x - fb->base_position.pose.position.x, 2) + pow(goal.at(currentPoseIndex).target_pose.pose.position.y - fb->base_position.pose.position.y, 2)) < 2)
                 {
                     //...cancel the current goal
                     needToCancelGoal = true;
-                    //ROS_INFO_STREAM("Cancelling goal at (" << goal.at(currentPoseIndex).target_pose.pose.position.x << ", " << goal.at(currentPoseIndex).target_pose.pose.position.y << "because a mail box is in the way");
                 }
     }
 }
@@ -125,14 +158,18 @@ int main(int argc,char **argv) {
     while (!ac.waitForServer()) {}
     ROS_INFO_STREAM("done!");
 
+    ROS_INFO_STREAM("Waiting for global costmap to be avaliable...");
+    while (!receivedGlobal) {
+        spinOnce();
+    }
+    ROS_INFO_STREAM("done!");
+
     createGoalPoints();
 
     while (ok())
     {
         if (!sentPose.at(currentPoseIndex))
         {
-            ROS_INFO_STREAM("Sending goal point (" << goal.at(currentPoseIndex).target_pose.pose.position.x << ", " 
-                            << goal.at(currentPoseIndex).target_pose.pose.position.y << ")");
             ac.sendGoal(goal.at(currentPoseIndex),&serviceDone,&serviceActivated,&serviceFeedback);
             sentPose.at(currentPoseIndex) = true;
         }
@@ -149,101 +186,4 @@ int main(int argc,char **argv) {
     
     ROS_INFO_STREAM("Exiting");
     return 0;    
-}
-
-void createGoalPoints()
-{
-    move_base_msgs::MoveBaseGoal goalTemp;
-
-    goalTemp.target_pose.header.frame_id = "map";
-    goalTemp.target_pose.header.stamp = Time::now();
-    goalTemp.target_pose.pose.orientation.x = 0;
-    goalTemp.target_pose.pose.orientation.y = 0;
-    goalTemp.target_pose.pose.orientation.z = 0;
-    goalTemp.target_pose.pose.orientation.w = 1;
-    
-    goalTemp.target_pose.pose.position.x = -8;
-    goalTemp.target_pose.pose.position.y = -8;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-    goalTemp.target_pose.pose.position.y = -4;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-    goalTemp.target_pose.pose.position.y = 0;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-    goalTemp.target_pose.pose.position.y = 4;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-    goalTemp.target_pose.pose.position.y = 8;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-
-    goalTemp.target_pose.pose.position.x = -4;
-    goalTemp.target_pose.pose.position.y = 8;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-    goalTemp.target_pose.pose.position.y = 4;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-    goalTemp.target_pose.pose.position.y = 0;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-    goalTemp.target_pose.pose.position.y = -4;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-    goalTemp.target_pose.pose.position.y = -8;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-
-    goalTemp.target_pose.pose.position.x = 0;
-    goalTemp.target_pose.pose.position.y = -8;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-    //goalTemp.target_pose.pose.position.y = -4;
-    //goal.push_back(goalTemp);
-    //sentPose.push_back(false);
-    goalTemp.target_pose.pose.position.y = 0;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-    goalTemp.target_pose.pose.position.y = 4;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-    goalTemp.target_pose.pose.position.y = 8;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-
-    goalTemp.target_pose.pose.position.x = 4;
-    goalTemp.target_pose.pose.position.y = 8;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-    goalTemp.target_pose.pose.position.y = 4;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-    goalTemp.target_pose.pose.position.y = 0;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-    goalTemp.target_pose.pose.position.y = -4;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-    goalTemp.target_pose.pose.position.y = -8;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-
-    goalTemp.target_pose.pose.position.x = 8;
-    goalTemp.target_pose.pose.position.y = -8;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-    goalTemp.target_pose.pose.position.y = -4;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-    goalTemp.target_pose.pose.position.y = 0;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-    goalTemp.target_pose.pose.position.y = 4;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
-    goalTemp.target_pose.pose.position.y = 8;
-    goal.push_back(goalTemp);
-    sentPose.push_back(false);
 }
